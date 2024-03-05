a5e9beec8a3b507369d17e0bac13c415df169f59
time <time@netlight.com>
2024-03-05 17:01:43 +0100

    Add json file with 'data', footer and some styling updates

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-const menu = document.getElementById('hamburger-menu')
+// import people from './data/people'
 
-menu.addEventListener('click', () => {
-  console.log('click')
+const hamburgerMenu = document.getElementById('hamburger-menu')
+const menu = document.getElementById('nav-menu')
+
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active')
   menu.classList.toggle('active')
 })
+
+window.onload = function () {
+  const loadData = () => {
+    fetch('people.json')
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          handlePeopleData(data)
+        }
+      })
+      .catch((error) => console.log(error))
+  }
+
+  // Call loadData function with handleData as the callback
+  loadData()
+}
+
+const handlePeopleData = (data) => {
+  const aboutSection = document.getElementById('about-section-inner')
+  const people = data.people
+
+  if (people) {
+    personHTML = people.forEach((person) => {
+      const el = document.createElement('div')
+      el.classList.add('about-person')
+      el.innerHTML = `<img src="./icons/logo-neo-secondary.png"/><p>${person.name}</p>`
+      aboutSection.appendChild(el)
+    })
+  }
+}
